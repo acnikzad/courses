403c10ac8990c23401d33122c205fb5c18f4f364
Aria Nikzad <a.c.nikzad@gmail.com>
2022-06-05 12:23:31 -0700

    finished teacher controller

--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -5,19 +5,41 @@ class Api::TeachersController < ApplicationController
   end
 
   def show
-
     the_id = params[:id]
     first_name: params[:first_name]
     last_name: params[:last_name]
+    course_id: params[:course_id]
     render jason: @teacher
   end
 
   def create
-    @teacher =Teacher.create (
-      label
-
+    @teacher = Teacher.create (
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      course_id: params[:course_id]
       )
+    @teacher.save
+    render:show
   end
 
+  def update
+    the_id = params[:id]
+    @teacher = Teacher.find_by(id: the_id)
+    @teacher.first_name = params[:first_name] || @teacher.first_name
+    @teacher.last_name = params[:last_name] || @teacher.last_name
+    @teacher.course_id = params[:course_id] || @teacher.course_id
 
+    @teacher.save
+    render:show
+  end
+
+  def destroy
+    @teacher = Teacher.find_by(id: params[:id])
+
+    if @teacher.present?
+      @teacher.destroy
+      @teacher.save
+    end
+    render
+  end
 end
