09c31981a7e931702b7afc10edb34e565daf9252
Aria Nikzad <a.c.nikzad@gmail.com>
2022-06-05 13:30:47 -0700

    finished views

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,2 +1,44 @@
 class Api::CoursesController < ApplicationController
+
+  def index
+    @courses = Course.all
+  end 
+
+  def show
+    the_id = params[:id]
+    @course = Course.find_by(id: the_id)
+    render json: @course
+  end
+
+  def create
+    @course = Course.create (
+      name: params[:name],
+      teacher_id: params [:teacher_id],
+      student_id: params [:student_id]
+      )
+    @course.save
+    render :show
+  end
+
+  def update
+    the_id = params[:id]
+    @course = Course.find_by(id: the_id)
+    @course.name = params[:name] || @course.name
+    @course.teacher_id = params[:teacher_id] || @course.teacher_id
+    @course.student_id = params[:student_id] || @course.student_id
+
+    @course.save
+    render :show
+  end
+
+  def destroy
+    @course = Course.find_by(id: params[:id])
+
+    if @course.present?
+      @course.destroy
+      @course.save
+    end
+
+    render
+  end
 end
