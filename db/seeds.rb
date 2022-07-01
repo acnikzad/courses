8dd94056e2c70d3bb8b73afa10a4063d038d66e4
Aria Nikzad <a.c.nikzad@gmail.com>
2022-07-01 16:39:19 -0700

    uncommented seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,76 +11,76 @@ f_names = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fr
 
 l_names = ["Donnelly", "Elrod", "Foust", "Griswold", "Simmons", "Milam", "Lance", "Peacock", "Byrne", "Yeager", "Barone", "Worden", "Wylie", "Cantu", "Bello", "Whatley", "George", "Bland", "Larue", "Ceballos", "Ritter", "Meeks", "Cho", "Artis", "Suggs", "Rader", "Ramsey", "Randolph", "Benavides", "Culbertson", "Argueta", "Hahn", "Forster", "Chew", "Turpin", "Behrens", "Garrett", "Upton", "Frost", "Prado", "Rojas", "Wenger", "DeJesus", "Barnett", "Callahan", "Simms", "Griggs", "Early", "Valencia", "Giron", "Cowart", "Eckert", "Stern", "Sadler", "Hardin", "Villarreal", "Gardiner", "Swisher", "Branson", "Weller", "Calvin", "Frye", "Whiteside", "Ridgeway", "Grogan", "Osorio", "Strand", "Whiting", "Kendrick", "Hartley", "Rincon", "Gale", "Forsythe", "McMahan", "Nikzad," "Vargas", "Cheney", "Iverson", "Engle", "Neuman", "Roman", "Pak", "Farley", "Royal", "Davila", "Packard", "Hollins", "Negrete", "Fortune", "Ontiveros", "Augustine", "Zuniga", "Clarke", "Hills", "Granger", "Waggoner", "Witte", "Warden", "Kohn", "Thacker", "Shen", "Kirkpatrick", "Contreras", "Harder", "Waite", "Durbin", "Looney", "Vega", "Dykes", "Sanborn", "Penny", "Fowler", "Sargent", "Lovett", "Lujan", "StClair", "Santiago", "Sharpe", "Nagy", "Edmondson", "Rucker", "Nixon", "Kessler", "Kuykendall", "Dorman", "Pappas", "Gilliam", "Barksdale", "Beal", "Comstock", "Borges", "Phan", "Swope", "Rushing", "Pierson", "Mayes", "Altman", "Alvarez", "Yanez", "Everett", "Welsh", "German", "Bingham", "Cloud", "Delagarza", "Connelly", "Stubblefield", "Jaime", "Hook", "Dover", "Hutchings", "Cardenas", "McIntyre", "Cole", "Alderman", "Zavala", "Earley", "Clayton", "Crandall", "Blakely", "Cardona", "Berman", "Conley", "Winters", "Hammons", "Morales", "Martins", "Madrid", "Koontz", "Salinas", "Windham", "Jaeger", "Pineda", "Fitzsimmons", "Stine", "Thurman", "Donnell", "Marlow", "Poirier", "Peebles", "Jaramillo", "Dawkins", "McDermott", "Connor", "Wasson", "Wheaton", "Ennis", "Sturgeon", "Bailey", "Perrin", "Ainsworth", "Ewing", "Massey", "Lauer", "Hodge", "Ballard", "Corbin", "Reedy", "Dobbs", "Fairbanks", "Brito"]
 
-# student = Student.all
+student = Student.all
 
-# 100.times do
-#   random_f_name = f_names[rand(0..f_names.length)]
-#   random_l_name = l_names[rand(0..l_names.length)]
-#   @student = Student.create({
-#     "first_name" => random_f_name,
-#     "last_name" => random_l_name})
-#   end
-# puts Student.all 
+100.times do
+  random_f_name = f_names[rand(0..f_names.length)]
+  random_l_name = l_names[rand(0..l_names.length)]
+  @student = Student.create({
+    "first_name" => random_f_name,
+    "last_name" => random_l_name})
+  end
+puts Student.all 
 
-# teacher = Teacher.all
+teacher = Teacher.all
 
-# 30.times do
-#   random_f_name = f_names[rand(0..f_names.length)]
-#   random_l_name = l_names[rand(0..l_names.length)]
-#   @teacher = Teacher.create({
-#     "first_name" => random_f_name,
-#     "last_name" => random_l_name})
-#   puts random_f_name
-#   puts random_l_name
-#   end
-# puts Teacher.all 
+30.times do
+  random_f_name = f_names[rand(0..f_names.length)]
+  random_l_name = l_names[rand(0..l_names.length)]
+  @teacher = Teacher.create({
+    "first_name" => random_f_name,
+    "last_name" => random_l_name})
+  puts random_f_name
+  puts random_l_name
+  end
+puts Teacher.all 
 
-# course_prefix = ["Bus", "Fin", "Econ", "Acct", "Art", "Calc", "Math", "Geog", "Geol", "Ast", "Bio", "BusTec", "Chem", "Elect", "PolySci", "Eng", "Fire", "Hist", "Hort", "Hum", "IT", "Span", "Jour", "Mus", "Libr", "Coun", "Mkt", "Mgmt", "Nurs", "Psyc", "RE", "Spch", "Stat", "Anth", "Comm"]
+course_prefix = ["Bus", "Fin", "Econ", "Acct", "Art", "Calc", "Math", "Geog", "Geol", "Ast", "Bio", "BusTec", "Chem", "Elect", "PolySci", "Eng", "Fire", "Hist", "Hort", "Hum", "IT", "Span", "Jour", "Mus", "Libr", "Coun", "Mkt", "Mgmt", "Nurs", "Psyc", "RE", "Spch", "Stat", "Anth", "Comm"]
 
-# course_suffix = ["100", "101", "102", "103","200", "201", "202", "203", "300", "301", "302", "303", "400", "401", "402", "403", "500", "501", "502", "503"]
-# course_array = []
-
-
-# course = Course.all
-
-# 50.times do
-#   random_prefix = course_prefix[rand(0..course_prefix.length - 1)]
-#   random_suffix = course_suffix[rand(0..course_suffix.length - 1)]
-#   course_name = "#{random_prefix}#{random_suffix}"
-
-#   if course_name != course_array
-#     @course = Course.create({
-#       "name" => course_name})
-#     course_array << course_name
-#   end
-# end
-
-# puts Course.all
+course_suffix = ["100", "101", "102", "103","200", "201", "202", "203", "300", "301", "302", "303", "400", "401", "402", "403", "500", "501", "502", "503"]
+course_array = []
 
 
-# x = 1
+course = Course.all
 
-# while x < Student.count
-#   student = Student.find_by(id: x)
-#   student.courses << Course.find(rand(2..51))
-#   student.courses << Course.find(rand(2..51))
-#   student.courses << Course.find(rand(2..51))
-#   x += 1
-#   puts student.courses
-# end
+50.times do
+  random_prefix = course_prefix[rand(0..course_prefix.length - 1)]
+  random_suffix = course_suffix[rand(0..course_suffix.length - 1)]
+  course_name = "#{random_prefix}#{random_suffix}"
+
+  if course_name != course_array
+    @course = Course.create({
+      "name" => course_name})
+    course_array << course_name
+  end
+end
+
+puts Course.all
 
 
-# y = 1
-# z = 51
+x = 1
 
-# while y < Teacher.count
-#   teacher = Teacher.find_by(id: y)
-#   teacher.courses << Course.find(y + 1)
-#   teacher.courses << Course.find(z)
-#   y += 1
-#   z -= 1
-#   puts teacher.courses
-# end
+while x < Student.count
+  student = Student.find_by(id: x)
+  student.courses << Course.find(rand(2..51))
+  student.courses << Course.find(rand(2..51))
+  student.courses << Course.find(rand(2..51))
+  x += 1
+  puts student.courses
+end
+
+
+y = 1
+z = 51
+
+while y < Teacher.count
+  teacher = Teacher.find_by(id: y)
+  teacher.courses << Course.find(y + 1)
+  teacher.courses << Course.find(z)
+  y += 1
+  z -= 1
+  puts teacher.courses
+end
 
 
 
