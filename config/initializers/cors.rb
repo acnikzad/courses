2895b7a2b5fe08b91bdee6f3da7edca46df37f60
Aria Nikzad <a.c.nikzad@gmail.com>
2022-07-04 08:11:35 -0700

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'courses-rb.herokuapp.com/', 'https://courses-vue.herokuapp.com/'
+    origins 'localhost:8080', 'courses-rb.herokuapp.com', 'https://courses-vue.herokuapp.com'
 
     resource '*',
       headers: :any,
